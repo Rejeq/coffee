8a73f66aa16e4c6d698ec98f68b66f045af623cf
Rejeq <jere3690@gmail.com>
2025-07-27 16:30:04 +0300

    add: Two lines to the top bar

--- app/src/main/java/com/rejeq/sws/ui/utils/SwsTopBar.kt
+++ app/src/main/java/com/rejeq/sws/ui/utils/SwsTopBar.kt
@@ -1,6 +1,10 @@
 package com.rejeq.sws.ui.utils
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -9,19 +13,45 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewDynamicColors
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import com.rejeq.sws.R
 import com.rejeq.sws.ui.theme.SwsTheme
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
 fun SwsTopBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    onBackClick: (() -> Unit)? = null,
+    actions: @Composable RowScope.() -> Unit = {},
+) {
+    Box(modifier = modifier) {
+        RawTopBar(
+            title = title,
+            onBackClick = onBackClick,
+            actions = actions,
+        )
+
+        TwoLines(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(),
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RawTopBar(
     title: String,
     modifier: Modifier = Modifier,
     onBackClick: (() -> Unit)? = null,
@@ -53,6 +83,28 @@ fun SwsTopBar(
         },
         actions = actions,
     )
+}
+
+@Composable
+fun TwoLines(modifier: Modifier = Modifier) {
+    val lineColor = MaterialTheme.colorScheme.surfaceTint
+
+    Canvas(
+        modifier = modifier.height(2.dp),
+    ) {
+        drawLine(
+            color = lineColor,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            strokeWidth = 1f,
+        )
+        drawLine(
+            color = lineColor,
+            start = Offset(0f, size.height),
+            end = Offset(size.width, size.height),
+            strokeWidth = 1f,
+        )
+    }
 }
 
 @Composable
